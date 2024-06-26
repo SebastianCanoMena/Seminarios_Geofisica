1bdbf1f399dffdf6f7a174c1a25e0c7178fe4300
SebastianCanoMena <sebastiancanomena@gmail.com>
2024-06-26 09:44:11 -0600

    Added routing

--- src/components/Card/type/Institutional.jsx
+++ src/components/Card/type/Institutional.jsx
@@ -1,40 +1,17 @@
-import { useState } from 'react';
-import { getCollection, getEntry} from 'astro:content'
-import Institutional from './type/Institutional'
-import DisplayCard from './DisplayCard';
+import { getEntry} from 'astro:content'
 
-/*Gets the data collection 'seminar', located at src/content/seminars*/
-const seminars = await getCollection('seminars');
-
-export default function Card({type})
-{
+export default function Institutional({seminars, currentDate, display, setDisplay, seminar, setSeminar, nextSeminar, setNextSeminar}){
     
-    const [display, setDisplay] = useState(false)
-    const [seminar, setSeminar] = useState(null)
-    const [nextSeminar, setNextSeminar] = useState(true)
-
-    /*Gets the user current date */
-    let currentDate = new Date()
-
-    /*Changes currentDate format, to the date format used in 'seminars' collection*/
-    currentDate = (currentDate.getFullYear()+ ' 0'+currentDate.getMonth()+ ' '+ 
-    currentDate.getDate()+' '+currentDate.getHours()+' '+currentDate.getMinutes())
-
-    /*Gets upcoming seminars*/ 
-    var seminarN = seminars.filter((a) => currentDate < a.data.datetime)
+    /*Gets the upcoming seminars*/
+    const seminarN = seminars.filter((a) => currentDate < a.data.datetime)
 
     /*Gets the past seminars*/
-    var seminarP = seminars.filter((a) => currentDate > a.data.datetime)
-    
-    if(type != "institutional"){
-        seminarN = seminarN.filter((a) => a.data.type == type)
+    const seminarP = seminars.filter((a) => currentDate > a.data.datetime)
 
-        seminarP = seminarP.filter((a) => a.data.type == type)
-    }
     
     let i = -1
     /*Maps the next seminars data to a jsx format*/
-    const Nextseminars = seminarN.map(seminar => (
+    const nextSeminars = seminarN.map(seminar => (
         <div key={i =+ 1} id={i==-1 ? 'next' : null} className={i==-1 ? 'card-div-parent-next' : 'card-div-parent'} 
         onClick={async () => {  
             setDisplay(!display) 
@@ -60,7 +37,7 @@ export default function Card({type})
 
     i = -1
     /*Maps the past seminars data to a jsx format*/
-    const PastSeminars = seminarP.map(seminar => (
+    const pastSeminars = seminarP.map(seminar => (
         <div key={i =+1}id={i==-1 ? 'past' : null} className='card-div-parent' 
         onClick={async () => {
             setDisplay(!display)
@@ -83,9 +60,9 @@ export default function Card({type})
         </div>
         </div>
         ))
-    
-    return(
-        <div className='cards'>
-            {(display && seminar!=null) ? <DisplayCard seminar={seminar} display={setDisplay} nextSeminar={nextSeminar}/> :<> {Nextseminars} {PastSeminars}</>}
-        </div> 
-    );}
+        return(
+            <>
+            <h1>dddd</h1>
+            </>
+        )
+}
